29cd7a10c0e95a80b84cc721556ff551dfec2d55
Sam Meech-Ward <sam@meech-ward.com>
2017-06-16 10:40:40 -0700

    Create a single page vc with a label
    Create a single page view controller in the main storyboard that has a single uilabel. We will end up using this label to show the current page number.

---
+++ Pagining/SinglePageViewController.h
@@ -0,0 +1,13 @@
+//
+//  SinglePageViewController.h
+//  Pagining
+//
+//  Created by Sam Meech-Ward on 2017-06-16.
+//  Copyright © 2017 Sam Meech-Ward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SinglePageViewController : UIViewController
+
+@end
---
+++ Pagining/SinglePageViewController.m
@@ -0,0 +1,38 @@
+//
+//  SinglePageViewController.m
+//  Pagining
+//
+//  Created by Sam Meech-Ward on 2017-06-16.
+//  Copyright © 2017 Sam Meech-Ward. All rights reserved.
+//
+
+#import "SinglePageViewController.h"
+
+@interface SinglePageViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@end
+
+@implementation SinglePageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
